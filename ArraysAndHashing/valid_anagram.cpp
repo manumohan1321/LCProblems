4dc3a2018cf91f5e90488fb96b7f2fa8ddf0dcb0
Manu Mohan <manumohan@Manus-MacBook-Pro.local>
2024-06-21 13:02:23 +0530

    added valid anagram problem

--- ArraysAndHashing/valid_anagram.cpp
+++ ArraysAndHashing/valid_anagram.cpp
@@ -1,22 +1,32 @@
-// LC 242
+// Problem Link: https://leetcode.com/problems/valid-anagram/description/
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        unordered_map<char, int> mpp;
+        unordered_map<char, int> s_mpp;
+        unordered_map<char, int> t_mpp;
 
-        for(char c : s) {
-            mpp[c] += 1;
+        if (s.size() != t.size()) {
+            return false;
         }
 
-        for(char c : t) {
-            mpp[c] -= 1;
+        for (int i=0; i<s.size(); i++) {
+            s_mpp[s[i]]++;
+            t_mpp[t[i]]++;
         }
 
-        for (auto& it: mpp) {
-            if (it.second != 0) return false;
+        for (int i=0; i<s_mpp.size(); i++) {
+            if (s_mpp[i] != t_mpp[i]) {
+                return false;
+            }
         }
 
         return true;
+
     }
 };
