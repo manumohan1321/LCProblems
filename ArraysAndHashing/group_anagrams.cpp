d5d4a2e8a7d96d32e49b6eeda1d12d37d90aadf1
Manu Mohan <manumohan@Manus-MacBook-Pro.local>
2024-06-22 17:46:47 +0530

    added question group anagrams

--- ArraysAndHashing/group_anagrams.cpp
+++ ArraysAndHashing/group_anagrams.cpp
@@ -1,29 +1,35 @@
-// LC - 49
+// Problem Link: https://leetcode.com/problems/group-anagrams/description/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
 
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
-        unordered_map<string, vector<string>> mpp;
+        
+        unordered_map<string, vector<string>> umpp;
+        vector<vector<string>> res;
 
         for (int i=0; i<strs.size(); i++) {
             string key = createOrGetKey(strs[i]);
-            mpp[key].push_back(strs[i]);
+            umpp[key].push_back(strs[i]);
         }
 
-        vector<vector<string>> result;
-
-        for (auto it = mpp.begin(); it != mpp.end(); it++) {
-            result.push_back(it->second);
+        for (auto it=umpp.begin(); it!=umpp.end(); it++) {
+            res.push_back(it->second);
         }
 
-        return result;
-
+        return res;
     }
 
 private:
     string createOrGetKey(string str) {
         vector<int> count(26);
+
         for (int j=0; j<str.size(); j++) {
             count[str[j] - 'a']++;
         }
